0110479a5c0a4fe94cf2399628521768b1b1eb17
Christian Heider Nielsen <mrnaah@gmail.com>
2017-08-09 13:54:45 +0200

    Dont sleep

--- simple-loop.py
+++ simple-loop.py
@@ -7,5 +7,4 @@ env = neo.NeodroidEnvironment(name='dodgescene.exe',connect_to_running=True)
 
 while(1):
   print('step')
-  env.step(Reaction(False,[]))#[Motion('Player','Wheel1Torque',5.)]))
-  #time.sleep(1)
+  print(env.step(Reaction(False,[Motion('Player','Wheel1Torque',5.)])))
